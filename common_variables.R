d2d4db2b99e92a1dbb10c369efc49260d90f4c62
Yi Xiong <390479311@qq.com>
2020-02-06 18:58:51 +0800

    First version

---
+++ common_variables.R
@@ -0,0 +1,19 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "I have over 3-year research experience in brain cancers such as glioma, brain metastatic cancers. I'm of great interest in using bioinformatics knowledge to understand the biology of cancer including pathogenesis, also the translational significance in the clinical practice.
+"
+
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  5,
+  "Javascript (d3.js)", 4.5,
+  "C++",                4,
+  "Python",             4,
+  "Bash",               3.5,
+  "SQL",                3,
+  "AWK",                3
+)
